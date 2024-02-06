711f1cee413b8bf876297f2410dc304912d7a5c4
TheRealFilko <fn.filipnikolic@gmail.com>
2024-02-06 21:20:10 +0100

    Change colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,9 +3,10 @@ require("therealfilko.lazy")
 
 -- Themes
 gruber = "gruber-darker"
+catppuccin = "catppuccin"
 
 function ColorMyPencils(color)
-    color = color or gruber 
+    color = color or catppuccin 
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
