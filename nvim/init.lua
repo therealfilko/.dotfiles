111075f6e2fdc02d6be19c3855eca8a7f4dfb35c
TheRealFilko <fn.filipnikolic@gmail.com>
2024-04-21 01:35:15 +0200

    changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,11 +3,11 @@ require("therealfilko.lazy")
 
 --local gruber = "gruber-darker"
 --local catppuccin = "catppuccin"
---local solarized = "solarized-osaka"
-local tokyonight = "tokyonight"
+--local tokyonight = "tokyonight"
+local gruvbox = "gruvbox"
 
 function ColorDeezNutz(color)
-    color = color or tokyonight
+    color = color or gruvbox
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
