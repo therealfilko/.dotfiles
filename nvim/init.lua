93126b3ae3dcc9b94fee103866a11bf003b23939
TheRealFilko <fn.filipnikolic@gmail.com>
2025-06-01 16:51:06 +0200

    style(theme switch): tokioo

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,11 +3,11 @@ require("therealfilko.lazy")
 
 --local gruber = "gruber-darker"
 --local catppuccin = "catppuccin"
---local tokyonight = "tokyonight"
-local gruvbox = "gruvbox-material"
+local tokyonight = "tokyonight"
+--local gruvbox = "gruvbox-material"
 
 function ColorDeezNutz(color)
-    color = color or gruvbox
+    color = color or tokyonight
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
